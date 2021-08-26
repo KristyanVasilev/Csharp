089a69263c79b37ffd89061a784ddf9d934bf32c
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-08-26 16:57:31 +0300

    Create cmToInches.cs

---
+++ cmToInches.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CMtoinches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cm = double.Parse(Console.ReadLine());
+
+            double inches = cm * 2.54;
+
+            Console.WriteLine(inches);
+        }
+    }
+}
