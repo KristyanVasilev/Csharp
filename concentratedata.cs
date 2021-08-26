9a6c5eb2101ba828acfda4a8237b40011f3af8f0
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-08-26 16:57:35 +0300

    Create concentratedata.cs

---
+++ concentratedata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace concentratedata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            string lastname = Console.ReadLine();
+
+            int age = int.Parse(Console.ReadLine());
+
+            string town = Console.ReadLine();
+
+            Console.WriteLine($"You are {name} {lastname}, a {age}-years old person from {town}.");
+        }
+    }
+}
