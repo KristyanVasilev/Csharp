9d9ec2625404b197329905ecd2b877cda2baa22b
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-08-26 16:57:39 +0300

    Create greetingByName.cs

---
+++ greetingByName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Greeting_by_name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
