b49998b6bf494b879863f8885b0d9038f5447840
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-08-26 16:57:45 +0300

    Create myfirstporject.cs

---
+++ myfirstporject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace myfirstporject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int numProjects = int.Parse(Console.ReadLine());
+
+            int hours = numProjects * 3;
+
+            Console.WriteLine($"The architect {name} will need {hours} hours to complete {numProjects} project/s.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
