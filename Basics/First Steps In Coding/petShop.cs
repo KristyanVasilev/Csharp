e0d04199e600fb01227146384889786599003346
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-08-26 17:14:37 +0300

    First Steps

---
+++ Basics/First Steps In Coding/petShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PetShop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogNum = double.Parse(Console.ReadLine());
+            int othersPet = int.Parse(Console.ReadLine());
+            double dogFood = dogNum * 2.50;
+            int othersPetsF = othersPet * 4;
+
+            Console.WriteLine($"{dogFood + othersPetsF} lv.");
+        }
+    }
+}
---
+++ Basics/First Steps In Coding/yardGreening.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareGreen = double.Parse(Console.ReadLine());
+            double priceGreen = squareGreen * 7.61;
+           
+
+            double discount = priceGreen * 0.18;
+            
+
+
+            double finalPrice = priceGreen - discount;
+            Console.WriteLine($"The final price is: {finalPrice}lv.");
+            Console.WriteLine($"The discount is: {discount}lv.");
+
+        }
+    }
+}
