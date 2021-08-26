f772e219b70c9d4926e2dfb3e08d221e6aa29dba
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-08-26 16:53:00 +0300

    Create arraysExercises.cs

---
+++ arraysExercises.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace arraysExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days =
+                {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(days[i]);
+            }
+
+        }
+    }
+}
