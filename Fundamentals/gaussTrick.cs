f6bd5c1eacc94bbf149cc900fa8138fb595bd3f8
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-09-07 19:30:03 +0300

    fund

---
+++ Fundamentals/gaussTrick.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace gaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                int first = numbers[i];
+                int last = numbers[numbers.Count - 1 - i];
+                var resultNumber = first + last;
+
+                result.Add(resultNumber);
+            }
+
+            if (numbers.Count % 2 == 1)
+            {
+                var middleNumber = numbers[numbers.Count / 2];
+                result.Add(middleNumber);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ Fundamentals/listOfProducts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace listOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalProducts = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>(); 
+
+            for (int i = 0; i < totalProducts; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+
+            for (int j = 0; j < products.Count; j++)
+            {
+                Console.WriteLine($"{j + 1}. {products[j]}");
+            }
+        }
+    }
+}
