ce6d7a76fe052b8a825b3a000a3c999b141b7916
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-09-10 20:29:31 +0300

    Create objetcsAndClasses.cs

---
+++ Fundamentals/objetcsAndClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace objetcsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateAsText = Console.ReadLine();
+
+            DateTime date = DateTime.ParseExact(dateAsText, "d/M/yyyy", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(date.DayOfWeek);
+
+        }
+    }
+}
