bb84878bf1bdec8ea837c5774b794a001ef747a3
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-09-13 18:30:42 +0300

    loops

---
+++ Basics/Loops/agencyProfit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace agencyProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string companyName = Console.ReadLine();
+            int numOfAdultsTickets = int.Parse(Console.ReadLine());
+            int numOfKidTickets = int.Parse(Console.ReadLine());
+            double ticketPrice = double.Parse(Console.ReadLine());
+            double taxes = double.Parse(Console.ReadLine());
+
+            double kidTicket = ticketPrice - ticketPrice * 0.7;          
+
+            double adultTickets = (ticketPrice + taxes) * numOfAdultsTickets;
+           
+            double kidticketPrice = (kidTicket + taxes) * numOfKidTickets;
+          
+
+            double totalProfi = (adultTickets + kidticketPrice) *0.2 ;
+
+            Console.WriteLine($"The profit of your agency from {companyName} tickets is {totalProfi:f2} lv.");
+        }
+    }
+}
