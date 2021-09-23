0dc4be56c0aa646a0094783e457ba80a6c551a36
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-09-23 18:56:31 +0300

    dataTypes

---
+++ Fundamentals/Data Types and Variables - Lab/charToString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace charToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char fisrt = Console.ReadLine()[0];
+            Char second = Console.ReadLine()[0];
+            Char third = Console.ReadLine()[0];
+
+            Console.WriteLine($"{fisrt}{second}{third}");
+        }
+    }
+}
---
+++ Fundamentals/Data Types and Variables - Lab/lowerOrUpper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            char ch1 = Console.ReadLine()[0]; 
+            result = Char.IsUpper(ch1);
+            if (result == true)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
