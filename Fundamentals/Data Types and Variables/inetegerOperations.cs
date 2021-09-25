11afb98e10fed636cbd66f08bf86f4da9bf78ea1
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-09-25 22:40:47 +0300

    fund

---
+++ Fundamentals/Data Types and Variables/inetegerOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace integerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourth = int.Parse(Console.ReadLine());
+
+            int sum = ((first + second) / third) *fourth;
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Fundamentals/Data Types and Variables/sumDigits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double totalSum = 0;
+
+            while (number > 0)
+            {
+                int sum = number % 10;
+                number /= 10;
+                totalSum += sum;
+                sum = 0;
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
