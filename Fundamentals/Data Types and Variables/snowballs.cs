c3644681e3a613d5c9dc363e07f9fc2da7806d8c
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-09-29 20:14:16 +0300

    dataTypes

---
+++ Fundamentals/Data Types and Variables/snowballs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+namespace EX11._Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberSnowballs = int.Parse(Console.ReadLine());
+            BigInteger bestValue = 0;
+            int bestSnow = 0;
+            int bestTime = 0;
+            int bestQuality = 0;
+            for (int i = 0; i < numberSnowballs; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                if (snowballTime > 0)
+                {
+                    BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+                    if (bestValue <= snowballValue)
+                    {
+                        bestSnow = snowballSnow;
+                        bestTime = snowballTime;
+                        bestQuality = snowballQuality;
+                        bestValue = snowballValue;
+                    }
+                }
+            }
+            Console.WriteLine($"{bestSnow} : {bestTime} = {bestValue} ({bestQuality})");
+        }
+    }
+}
