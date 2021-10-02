7b6b7a6a86889a63ee05bf116ceb7e768f63ca7b
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-02 19:44:09 +0300

    Arrays

---
+++ Fundamentals/Arrays/condenseArrayToNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace condenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] condensed = new int[nums.Length];
+            int counter = 1;
+            int lenght = nums.Length;
+
+            if (lenght == 1)
+            {
+                Console.WriteLine(nums[0]);
+                return;
+            }
+
+            while (true)
+            {
+                for (int i = 0; i < nums.Length - counter; i++)
+                {
+                    condensed[i] = nums[i] + nums[i + 1];
+                }
+                counter++;
+
+                nums = condensed;
+                if (lenght == counter)
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine(condensed[0]);
+
+        }
+
+    }
+}
---
+++ Fundamentals/Arrays/equalArrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace equalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] array2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            //if (array1.Length != array2.Length)
+            //{
+
+            //}
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                sum += array1[i];
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
