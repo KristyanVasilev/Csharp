62ea74da8e47db0fc9d289cc6b08ff151bbf4b52
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-08 20:04:11 +0300

    Methods

---
+++ Fundamentals/Methods/T07. Repeat String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            RepeatString(input, n);
+        }
+
+        static void RepeatString(string input, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(input);
+            }
+        }
+    }
+}
