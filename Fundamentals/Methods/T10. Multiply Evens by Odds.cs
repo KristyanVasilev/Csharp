febc18e6ff1847c74bcbc50103a2d6714f6e0190
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-09 21:36:41 +0300

    methods

---
+++ Fundamentals/Methods/T10. Multiply Evens by Odds.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T10._Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Math.Abs(int.Parse(Console.ReadLine()));
+            int sum = 0;
+            int evenSum = 0;
+            int oddSum = 0;
+
+            while (input > 0)
+            {
+                sum = input % 10;
+                input /= 10;
+                if (sum % 2 == 0)
+                {
+                    evenSum += sum;
+                }
+                else
+                {
+                    oddSum += sum;
+                }
+            }
+            Console.WriteLine(GetMultipleOfEvenAndOdds(evenSum, oddSum));
+        }
+
+        static int GetMultipleOfEvenAndOdds(int evenSum, int oddSum)
+        {
+            int sum = evenSum * oddSum;
+            return sum;
+        }
+    }
+}
