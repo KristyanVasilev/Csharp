13a6d2195f26faeeada7e5364cb6ec154265040f
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-10 21:59:42 +0300

    methods

---
+++ Fundamentals/Methods/T11. Math operations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace T11._Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            string operatorr = Console.ReadLine();
+            int number2 = int.Parse(Console.ReadLine());
+
+            switch (operatorr)
+            {
+                case "*":
+                    Console.WriteLine(Multoply(number1, number2));
+                    break;
+
+                case "/":
+                    Console.WriteLine(Divide(number1, number2));
+                    break;
+
+                case "+":
+                    Console.WriteLine(add(number1, number2));
+                    break;
+
+                case "-":
+                    Console.WriteLine(Substract(number1, number2));
+                    break;
+
+            }           
+        }
+
+        private static int Divide(int number1, int number2)
+        {
+            return number1 /= number2;
+        }
+
+        private static int Substract(int number1, int number2)
+        {
+            return number1 -= number2;
+        }
+
+        private static int add(int number1, int number2)
+        {
+            return number1 += number2;
+        }
+
+        static int Multoply(int number1, int number2)
+        {
+            return number1 *= number2;
+        }
+    }
+}
