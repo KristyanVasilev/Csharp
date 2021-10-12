a067bce60f3f89a90db205be5df01e4a8da6e81e
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-12 18:30:32 +0300

    methods

---
+++ Fundamentals/Methods/T07. NxN Matrix.cs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace T07._NxN_Matrix.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintNxNMatrix(number);
+        }
+
+        private static void PrintNxNMatrix(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Fundamentals/Methods/T08. Factorial Division.ex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T08._Factorial_Division.ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = double.Parse(Console.ReadLine());
+            double number2 = double.Parse(Console.ReadLine());
+
+            double divideOfFact = PrintFacturial(number1, number2);
+            Console.WriteLine($"{Math.Abs(divideOfFact):f2}");
+        }
+
+        static double PrintFacturial(double number1, double number2)
+        {
+            double sumOfFirstFactorial = 1;
+            double sumOfSecondFactorial = 1;
+
+            for (double firstFact = number1; firstFact > 0; firstFact--)
+            {
+                sumOfFirstFactorial *= firstFact;
+            }
+
+            for (double secondFact = number2; secondFact > 0; secondFact--)
+            {
+                sumOfSecondFactorial *= secondFact;
+            }
+
+            double divideOfFact =  sumOfFirstFactorial  / sumOfSecondFactorial;
+            return divideOfFact;
+        }
+    }
+}
