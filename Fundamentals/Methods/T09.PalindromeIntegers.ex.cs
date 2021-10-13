fa78e1ac61151746be2273d62f4c90540a6b392d
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-13 22:34:34 +0300

    methods

---
+++ Fundamentals/Methods/T09.PalindromeIntegers.ex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T09.PalindromeIntegers.ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                Console.WriteLine(IsPalindrome(input));
+            }
+        }
+
+        private static bool IsPalindrome(string input)
+        {
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
