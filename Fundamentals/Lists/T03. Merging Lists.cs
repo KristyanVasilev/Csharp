d5a8e89a7df29f058dd28d7a3f08ae154eca0426
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-15 17:40:32 +0300

    lists

---
+++ Fundamentals/Lists/T03. Merging Lists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> list2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> mergedList = new List<int>();
+
+
+            int biggestLenght = list1.Count;
+            if (biggestLenght < list2.Count)
+            {
+                biggestLenght = list2.Count;
+            }
+
+            for (int i = 0; i < biggestLenght; i++)
+            {
+                if (list1.Count > i)
+                {
+                    mergedList.Add(list1[i]);
+                }
+
+                if (list2.Count > i)
+                {
+                    mergedList.Add(list2[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", mergedList));
+
+        }
+    }
+}
---
+++ Fundamentals/Lists/T05. Remove Negatives and Reverse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05._Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+                
+            }
+
+            numbers.Reverse();
+
+            if (numbers.Count <= 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+
+        }
+    }
+}
