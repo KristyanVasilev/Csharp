f0ebcbb7b6fa29776a940f85879f2d53e960ac36
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-25 23:48:40 +0300

    percents

---
+++ Fundamentals/ExamPreparation/differentPercents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 150 * 75 / 100; // *1.0
+            double b = 150 * 0.75;
+            double c = 150 / 100 * 75; // *1.0
+            bool isEqual = false;
+
+            if (a == b && a == c && b == c)
+            {
+                isEqual = true;
+            }
+            Console.WriteLine(a); //112 
+            Console.WriteLine(b); //112.5
+            Console.WriteLine(c); //75
+            Console.WriteLine(isEqual);
+        }
+    }
+}
