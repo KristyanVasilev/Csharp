85ccb36277efee9a8c069b8a781cce15ac49f1f2
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-26 09:15:01 +0300

    Create T09. Counter-Strike.cs

---
+++ Fundamentals/ExamPreparation/T09. Counter-Strike.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T09._Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            string enemyDistance = Console.ReadLine();
+            int count = 0;
+
+            while (enemyDistance != "End of battle")
+            {
+                int distance = int.Parse(enemyDistance);
+                if (energy >= distance)
+                {
+                    energy -= distance;
+                    count++;
+                    if (count % 3 == 0) energy += count;
+                }
+                else Console.WriteLine($"Not enough energy! Game ends with {count} won battles and {energy} energy");
+
+                enemyDistance = Console.ReadLine();
+            }
+            Console.WriteLine($"Won battles: {count}. Energy left: {energy}");
+        }
+    }
+}
