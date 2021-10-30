ee7ea8fad9c7492bf8eb00f3022d5a2c7d3d275b
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-10-30 23:32:30 +0300

    Create T01. Sort Numbers_MoreExe.cs

---
+++ Fundamentals/basics syntax/T01. Sort Numbers_MoreExe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T01._Sort_Numbers_MoreExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
