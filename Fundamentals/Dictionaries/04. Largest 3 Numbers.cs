90cc1292ce8e7885a023c12107731503f5ed35cf
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-11-06 17:06:51 +0200

    Dictionaries

---
+++ Fundamentals/Dictionaries/04. Largest 3 Numbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).Take(3).ToList();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
