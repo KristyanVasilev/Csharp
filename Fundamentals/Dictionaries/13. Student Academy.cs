55feb369605503c08f7a3d8a0a1b5276beaf8b11
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-11-10 10:41:20 +0200

    Dictionaries

---
+++ Fundamentals/Dictionaries/13. Student Academy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _13._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentsGrade = new Dictionary<string, List<double>>();
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (!studentsGrade.ContainsKey(name))
+                {
+                    studentsGrade.Add(name, new List<double>());
+                }
+                studentsGrade[name].Add(grade);
+            }
+
+            foreach (var grade in studentsGrade.OrderByDescending(x => x.Value.Average()))
+            {
+                if (grade.Value.Average() >= 4.5)
+                {
+                    Console.WriteLine($"{grade.Key} -> {grade.Value.Average():f2}");
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/Dictionaries/14. Courses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _14._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var SoftUniCourses = new Dictionary<string, List<string>>();
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                var cmd = input.Split(" : ");
+                string course = cmd[0];
+                string studentName = cmd[1];
+
+                if (!SoftUniCourses.ContainsKey(course))
+                {
+                    SoftUniCourses.Add(course, new List<string>());
+                }
+                SoftUniCourses[course].Add(studentName);
+                
+                input = Console.ReadLine();
+            }
+
+            foreach (var course in SoftUniCourses.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{course.Key.Trim()}: {course.Value.Count}");
+
+                foreach (var name in course.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {name}");
+                }
+            }
+        }
+    }
+}
