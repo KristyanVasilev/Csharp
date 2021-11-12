7ad1554d339e1de7d5584500b68e367de971d79c
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-11-12 20:24:39 +0200

    Text Processing

---
+++ Fundamentals/Text Processing/03. Substring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string text = Console.ReadLine();
+            int index = text.IndexOf(wordToRemove);
+
+            while (index >= 0)
+            {
+                text = text.Remove(index,wordToRemove.Length);
+                index = text.IndexOf(wordToRemove);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Fundamentals/Text Processing/04. Text Filter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] word = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var bannedword in word)
+            {
+                text = text.Replace(bannedword, new string('*', bannedword.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
