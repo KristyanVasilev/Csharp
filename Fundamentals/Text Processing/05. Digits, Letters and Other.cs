64c16dd55900e2f51f73a75eaba9de5a9ad58d72
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-11-15 22:56:05 +0200

    Text Processing

---
+++ Fundamentals/Text Processing/05. Digits, Letters and Other.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string letters = "";
+            string numbers = "";
+            string symbols = "";
+
+            foreach (char symbol in input)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    numbers += symbol;
+                }
+                else if (char.IsLetter(symbol))
+                {
+                    letters += symbol;
+                }
+                else
+                {
+                    symbols += symbol;
+                }
+            }
+
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(symbols);
+        }
+    }
+}
---
+++ Fundamentals/Text Processing/11. Replace Repeating Chars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _11._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char firstChar = input[0];
+            Console.Write(firstChar);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                char currChar = input[i];
+                if (currChar != firstChar)
+                {
+                    firstChar = currChar;
+                    Console.Write(firstChar);
+                }
+            }
+        }
+    }
+}
