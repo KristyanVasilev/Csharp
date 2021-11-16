2048adb3684539416698a94370754586b90b60ca
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-11-16 23:02:48 +0200

    Text Processing

---
+++ Fundamentals/Text Processing/12. String Explosion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _12._String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int bombDamage = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (bombDamage > 0 && text[i] != '>')
+                {
+                    text = text.Remove(i, 1); 
+                    bombDamage--; 
+                    i--; 
+                }
+                else if (text[i] == '>')
+                {
+                    bombDamage += int.Parse(text[i + 1].ToString());
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Fundamentals/Text Processing/13. Letters Change Numbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _13._String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            double sum = 0;
+
+            foreach (var item in input)
+            {
+                char firstLetter = item[0];
+                char lastLetter = item[^1];
+                string numAsString = item[1..^1];
+                double numFromstring = double.Parse(numAsString);
+
+                if (char.IsUpper(firstLetter))
+                {
+                    int position = firstLetter - 64;
+                    numFromstring /= position;
+                }
+                else
+                {
+                    int position = firstLetter - 96;
+                    numFromstring *= position;
+                }
+                if (char.IsUpper(lastLetter))
+                {
+                    int position = lastLetter - 64;
+                    numFromstring -= position;
+                }
+                else
+                {
+                    int position = lastLetter - 96;
+                    numFromstring += position;
+                }
+
+                sum += numFromstring;
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
