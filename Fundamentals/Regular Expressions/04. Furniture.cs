e311fe789a383e1ccb5ea7855aba7862d1d69750
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-11-20 19:26:27 +0200

    Regular Expressions

---
+++ Fundamentals/Regular Expressions/04. Furniture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _04._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double totalSum = 0;
+            int quantity = 0;
+            
+            Console.WriteLine("Bought furniture:");
+            while (input != "Purchase")
+            {
+                MatchCollection matches = Regex.Matches(input, @">>([A-Z][a-z]+|[A-Z]+)<<([0-9]+.[0-9]+|[0-9]+)!([0-9]+)");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Groups[1]);
+                    quantity = int.Parse(match.Groups[3].Value);
+                    totalSum += double.Parse(match.Groups[2].Value) * quantity;
+                    break;
+                }
+                quantity = 0;
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total money spend: {totalSum:f2}");
+        }
+    }
+}
