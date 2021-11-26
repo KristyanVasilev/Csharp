0168aae9e345dd1534cf538b8302f57af2012b6d
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-11-26 14:25:16 +0200

    examPreraration

---
+++ Fundamentals/ExamPreparation/T16. The Imitation Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace T16._The_Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder result = new StringBuilder(input);
+            string command = Console.ReadLine();
+
+            while (command != "Decode")
+            {
+                string[] splittedCommand = command.Split("|");
+                if (splittedCommand[0] == "Insert")
+                {
+                    int index = int.Parse(splittedCommand[1]);
+                    string value = splittedCommand[2];
+                    result.Insert(index, value);
+                }
+                else if (splittedCommand[0] == "Move")
+                {
+                    int numberOfLetters = int.Parse(splittedCommand[1]);
+                    string substring = result.ToString().Substring(0, numberOfLetters);
+                    result.Remove(0,numberOfLetters);
+                    result.Append(substring);
+                }
+                else if (splittedCommand[0] == "ChangeAll")
+                {
+                    result.Replace(splittedCommand[1], splittedCommand[2]);
+                }
+                command = Console.ReadLine();
+            }
+                Console.WriteLine($"The decrypted message is: {result}");
+        }
+    }
+}
---
+++ Fundamentals/ExamPreparation/T17.Detination mapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace T17.Setination_mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([=\/])(?<towns>[A-Z][A-Za-z]{2,})(\1)";
+            int travelPoints = 0;
+            List<string> destination = new List<string>();
+            bool isValid = false;
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                string town = match.Groups["towns"].Value;
+                destination.Add(town);
+                travelPoints += town.Length;
+                isValid = true;
+            }
+            if (isValid)
+            {
+                Console.Write("Destinations: ");
+                Console.WriteLine(string.Join(", ", destination));
+                Console.WriteLine($"Travel Points: {travelPoints}");
+            }
+            else
+            {
+                Console.WriteLine("Destinations: ");
+                Console.WriteLine($"Travel Points: {travelPoints}");
+            }
+        }
+    }
+}
