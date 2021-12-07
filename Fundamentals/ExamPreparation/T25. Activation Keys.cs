7d1e9321886b4a767727cf445c00218ca85e1add
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-12-08 00:06:11 +0200

    Create T25. Activation Keys.cs

---
+++ Fundamentals/ExamPreparation/T25. Activation Keys.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace T25._Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawActivationKey = Console.ReadLine();
+            string commands = Console.ReadLine();
+
+            while (commands != "Generate")
+            {
+                string[] currCmd = commands.Split(">>>");
+                if (currCmd[0] == "Contains")
+                {
+                    string substring = currCmd[1];
+                    if (rawActivationKey.Contains(substring))
+                    {
+                        Console.WriteLine($"{rawActivationKey} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (currCmd[0] == "Flip")
+                {
+                    string upperOrLower = currCmd[1];
+                    int startIndex = int.Parse(currCmd[2]);
+                    int endIndex = int.Parse(currCmd[3]);
+                    if (upperOrLower == "Upper")
+                    {
+                        rawActivationKey = rawActivationKey.Substring(0, startIndex) + rawActivationKey.Substring(startIndex, endIndex - startIndex).ToUpper() + rawActivationKey.Substring(endIndex);
+                        Console.WriteLine(rawActivationKey);
+                    }
+                    else
+                    {
+                        rawActivationKey = rawActivationKey.Substring(0, startIndex) + rawActivationKey.Substring(startIndex, endIndex - startIndex).ToLower() + rawActivationKey.Substring(endIndex);
+                        Console.WriteLine(rawActivationKey);
+                    }
+                }
+                else if (currCmd[0] == "Slice")
+                {
+                    int startIndex = int.Parse(currCmd[1]);
+                    int endIndex = int.Parse(currCmd[2]);
+                    rawActivationKey = rawActivationKey.Remove(startIndex, endIndex-startIndex);
+                    Console.WriteLine(rawActivationKey);
+                }
+                commands = Console.ReadLine();
+            }
+            Console.WriteLine($"Your activation key is: {rawActivationKey}");
+        }
+    }
+}
