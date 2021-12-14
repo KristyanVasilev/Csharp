f53cbc3e54882390fac7b692304c9a47d89d8367
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-12-14 13:24:47 +0200

    first steps in algorithms in c#

---
+++ Algorithms/Recursion and Backtracking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Recursion_and_Backtracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(SumRecursively(numbers,0));
+        }
+
+        private static int SumRecursively(int[] numbers, int index)
+        {
+            if (index == numbers.Length-1)
+            {
+                return numbers[index];
+            }
+            return numbers[index] += SumRecursively(numbers, index + 1);
+        }
+    }
+}
