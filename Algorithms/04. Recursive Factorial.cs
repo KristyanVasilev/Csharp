505433e744badd3d73b0e8d0ea77444622c70344
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-12-17 12:26:23 +0200

    Create 04. Recursive Factorial.cs

---
+++ Algorithms/04. Recursive Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalcFacturielRecur(n));
+        }
+
+        private static int CalcFacturielRecur(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * CalcFacturielRecur(n - 1);
+        }
+    }
+}
