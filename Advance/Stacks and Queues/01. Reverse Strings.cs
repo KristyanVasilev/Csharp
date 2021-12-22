c70b92081caf94829a53b13023bb304b4a7ae215
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-12-22 19:06:08 +0200

    Stacks and Queues

---
+++ Advance/Stacks and Queues/01. Reverse Strings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> myStack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                myStack.Push(ch);
+            }
+
+            while (myStack.Count > 0)
+            {
+                Console.Write(myStack.Pop());
+            }
+        }
+    }
+}
---
+++ Advance/Stacks and Queues/02. Stack Sum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> numbers = new Stack<int>();
+            foreach (var num in input)
+            {
+                numbers.Push(num);
+            }
+
+            string command = Console.ReadLine().ToLower();
+            while (command != "end")
+            {
+                string[] currCmd = command.Split();
+                if (currCmd[0] == "add")
+                {
+                    numbers.Push(int.Parse(currCmd[1]));
+                    numbers.Push(int.Parse(currCmd[2]));
+                }
+                else if (currCmd[0] == "remove")
+                {
+                    int elemtsToRemove = int.Parse(currCmd[1]);
+                    if (elemtsToRemove <= numbers.Count)
+                    {
+                        for (int i = 0; i < elemtsToRemove; i++)
+                        {
+                            numbers.Pop();
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {numbers.Sum()}");
+        }
+    }
+}
