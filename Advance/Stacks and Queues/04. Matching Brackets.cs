e4653ce24455fdc3a100e86c083577d9f9c6aa6d
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-12-24 15:15:01 +0200

    Create 04. Matching Brackets.cs

---
+++ Advance/Stacks and Queues/04. Matching Brackets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> myStack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    myStack.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    var openIndex = myStack.Pop();
+                    var closeIndex = i;
+                    Console.WriteLine(expression.Substring(openIndex, closeIndex - openIndex + 1));
+                }
+            }
+        }
+    }
+}
