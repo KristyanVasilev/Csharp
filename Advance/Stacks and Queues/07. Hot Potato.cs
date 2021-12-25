950313db766463ac718c41c133eaedd86651ae68
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-12-25 21:20:00 +0200

    Create 07. Hot Potato.cs

---
+++ Advance/Stacks and Queues/07. Hot Potato.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _07._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> kids = new Queue<string>();
+
+            foreach (var child in children)
+            {
+                kids.Enqueue(child);
+            }
+
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < n-1; i++)
+                {
+                    var potatoKid = kids.Dequeue();
+                    kids.Enqueue(potatoKid);
+                }
+
+                Console.WriteLine($"Removed {kids.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
