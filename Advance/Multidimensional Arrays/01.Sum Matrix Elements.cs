030da897c61ca4fb8c00fd63899cb2994ab47a10
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-12-28 19:20:25 +0200

    Create 01.Sum Matrix Elements.cs

---
+++ Advance/Multidimensional Arrays/01.Sum Matrix Elements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {               
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
