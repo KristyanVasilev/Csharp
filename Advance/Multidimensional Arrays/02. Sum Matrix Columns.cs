00460e95c74d8004f4d639bd872467998e17b37d
KristyanVasilev <kristiyanvasilev02@gmail.com>
2021-12-29 23:42:46 +0200

    Create 02. Sum Matrix Columns.cs

---
+++ Advance/Multidimensional Arrays/02. Sum Matrix Columns.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sumColums = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sumColums += matrix[row, col];
+                }
+                Console.WriteLine(sumColums);
+            }
+        }
+    }
+}
