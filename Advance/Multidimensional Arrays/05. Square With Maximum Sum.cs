66813085358dfd9d5c059e88e4f2f0bc34826e08
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-01-01 18:14:48 +0200

    Create 05. Square With Maximum Sum.cs

---
+++ Advance/Multidimensional Arrays/05. Square With Maximum Sum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Square_With_Maximum_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];          
+            long maxSum = long.MinValue;
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            int maxSumRow = 0;
+            int maxSumCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxSumRow = row;
+                        maxSumCol = col;
+                    }
+                }
+            }
+
+            for (int row = maxSumRow; row < maxSumRow + 2; row++)
+            {
+                for (int col = maxSumCol; col < maxSumCol + 2; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
