735acc257b63e1b528fa514a4511b9d780975c8e
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-01-05 22:16:46 +0200

    Create 06. Supermarket.cs

---
+++ Advance/Stacks and Queues/06. Supermarket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> people = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    foreach (var name in people)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    people.Clear();
+                }
+                else
+                {
+                    var name = input;
+                    people.Enqueue(name);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{people.Count} people remaining.");
+        }
+    }
+}
