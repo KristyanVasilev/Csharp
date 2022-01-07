8aac42517c5ae451a8ce04470b9aaf8a6a45e873
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-01-07 11:28:02 +0200

    Create 07. Pascal Triangle.cs

---
+++ Advance/Multidimensional Arrays/07. Pascal Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] pascal = new long[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                pascal[i] = new long[i + 1];
+                pascal[i][0] = 1;
+                pascal[i][pascal[i].Length - 1] = 1;
+                for (int j = 1; j < pascal[i].Length - 1; j++)
+                {
+                    pascal[i][j] = pascal[i - 1][j] + pascal[i - 1][j - 1];
+                }
+            }
+
+            for (int i = 0; i < pascal.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", pascal[i]));
+            }
+        }
+    }
+}
