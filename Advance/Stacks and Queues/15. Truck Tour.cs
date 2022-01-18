b8a840622db45948856385c0f641b14bc422e659
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-01-18 20:28:46 +0200

    stack and queues

---
+++ Advance/Stacks and Queues/15. Truck Tour.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15._Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int[]> truckTour = new Queue<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                truckTour.Enqueue(input);
+            }
+
+            int startIndex = 0;
+
+            while (true)
+            {
+                int currPetrol = 0;
+                foreach (var tour in truckTour)
+                {
+                    int petrolAmount = tour[0];
+                    int truckDistance = tour[1];
+
+                    currPetrol += petrolAmount;
+                    currPetrol -= truckDistance;
+                    if (currPetrol < 0)
+                    {
+                        int[] petrolStation = truckTour.Dequeue();
+                        truckTour.Enqueue(petrolStation);
+                        startIndex++;
+                        break;
+                    }
+                }
+
+                if (currPetrol >= 0)
+                {
+                    Console.WriteLine(startIndex);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Advance/Stacks and Queues/16. Balanced Parenthesis.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _16._Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<char> paranthese = new Stack<char>();
+            bool isValid = true;
+
+            for (int i = 0; i < expression.Length; i++)
+            {               
+                if (expression[i] == '{' || expression[i] == '[' || expression[i] == '(')
+                {
+                    paranthese.Push(expression[i]);
+                }
+                else
+                {
+                    if (paranthese.Count == 0)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    else if (expression[i] == ')' && paranthese.Peek() == '(') paranthese.Pop();
+                    else if (expression[i] == ']' && paranthese.Peek() == '[') paranthese.Pop();
+                    else if (expression[i] == '}' && paranthese.Peek() == '{') paranthese.Pop();
+                    else isValid = false;
+                }
+            }
+            Console.WriteLine(isValid? "YES" : "NO");
+        }
+    }
+}
