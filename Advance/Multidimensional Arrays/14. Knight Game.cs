02a36d8c5eb352e39bf464948f7bcaba0b1e61b9
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-01-22 23:26:06 +0200

    matrix

---
+++ Advance/Multidimensional Arrays/14. Knight Game.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace _14._Knight_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int boardSize = int.Parse(Console.ReadLine());
+            char[,] chessBoard = ReadBoard(boardSize, boardSize);
+            int attackedKnights = 0;
+            while (true)
+            {
+                int maxAttack = 0;
+                int rows = 0;
+                int cols = 0;
+                for (int row = 0; row < chessBoard.GetLength(0); row++)
+                {
+                    for (int col = 0; col < chessBoard.GetLength(1); col++)
+                    {
+                        if (chessBoard[row, col] == 'K') ;
+                        {
+                            int currAttack = 0;
+                            //upLeft, upRight
+                            if (IsValid(chessBoard, row - 2, col - 1) && chessBoard[row - 2, col - 1] == 'K')
+                            {
+                                currAttack++;
+                            }
+                            if (IsValid(chessBoard, row - 2, col + 1) && chessBoard[row - 2, col + 1] == 'K')
+                            {
+                                currAttack++;
+                            }
+                            //leftUp, leftDown
+                            if (IsValid(chessBoard, row - 1, col - 2) && chessBoard[row - 1, col - 2] == 'K')
+                            {
+                                currAttack++;
+                            }
+                            if (IsValid(chessBoard, row + 1, col - 2) && chessBoard[row + 1, col - 2] == 'K')
+                            {
+                                currAttack++;
+                            }
+                            //downLeft, downRight
+                            if (IsValid(chessBoard, row + 2, col - 1) && chessBoard[row + 1, col - 1] == 'K')
+                            {
+                                currAttack++;
+                            }
+                            if (IsValid(chessBoard, row + 2, col + 1) && chessBoard[row + 2, col + 1] == 'K')
+                            {
+                                currAttack++;
+                            }
+                            //rightUp, rightDown
+                            if (IsValid(chessBoard, row + 1, col + 2) && chessBoard[row + 1, col + 2] == 'K')
+                            {
+                                currAttack++;
+                            }
+                            if (IsValid(chessBoard, row - 1, col + 2) && chessBoard[row - 1, col + 2] == 'K')
+                            {
+                                currAttack++;
+                            }
+                            if (currAttack > maxAttack)
+                            {
+                                maxAttack = currAttack;
+                                rows = row;
+                                cols = col;
+                            }
+                        }
+                    }
+                }
+                if (maxAttack > 0)
+                {
+                    attackedKnights++;
+                    chessBoard[rows, cols] = '0';
+                }
+                else
+                {
+                    Console.WriteLine(attackedKnights); 
+                    break;
+                }
+
+            }
+        }
+
+        private static bool IsValid(char[,] chessBoard, int row, int col)
+        {
+            return row >= 0 && row < chessBoard.GetLength(0) && col >= 0 && col < chessBoard.GetLength(1);
+        }
+
+        private static char[,] ReadBoard(int rows, int cols)
+        {
+            char[,] chessBoard = new char[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                string input = Console.ReadLine();
+                for (int col = 0; col < cols; col++)
+                {
+                    chessBoard[row, col] = input[col];
+                }
+            }
+            return chessBoard;
+        }
+    }
+}
---
+++ Advance/Multidimensional Arrays/15. Bombs.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+
+namespace _15._Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = ReadMatrix(size, size);
+            int[] bombs = Console.ReadLine()
+                 .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            for (int i = 0; i < bombs.Length - 1; i+=2)
+            {
+                int row = bombs[i];
+                int col = bombs[i + 1];
+
+                if (matrix[row, col] <= 0) continue;
+
+                BombTheMatrix(matrix, row, col);
+            }
+            PrintAliveCellsAndSum(matrix);
+            PrintMatrix(matrix);
+        }
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void PrintAliveCellsAndSum(int[,] matrix)
+        {
+            int count = 0;
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        count++;
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine($"Alive cells: {count}");
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        private static void BombTheMatrix(int[,] matrix, int rows, int cols)
+        {
+            //top
+            if (IsInRange(matrix, rows - 1, cols) && matrix[rows - 1, cols] > 0)
+            {
+                matrix[rows - 1, cols] -= matrix[rows, cols];
+            }
+            if (IsInRange(matrix, rows - 1, cols - 1) && matrix[rows - 1, cols - 1] > 0)
+            {
+                matrix[rows - 1, cols - 1] -= matrix[rows, cols];
+            }
+            if (IsInRange(matrix, rows - 1, cols + 1) && matrix[rows - 1, cols + 1] > 0)
+            {
+                matrix[rows - 1, cols + 1] -= matrix[rows, cols];
+            }
+            //mid
+            if (IsInRange(matrix, rows, cols + 1) && matrix[rows, cols + 1] > 0)
+            {
+                matrix[rows, cols + 1] -= matrix[rows, cols];
+            }
+            if (IsInRange(matrix, rows, cols - 1) && matrix[rows, cols - 1] > 0)
+            {
+                matrix[rows, cols - 1] -= matrix[rows, cols];
+            }
+            //bot
+            if (IsInRange(matrix, rows + 1, cols) && matrix[rows + 1, cols] > 0)
+            {
+                matrix[rows + 1, cols] -= matrix[rows, cols];
+            }
+            if (IsInRange(matrix, rows + 1, cols - 1) && matrix[rows + 1, cols - 1] > 0)
+            {
+                matrix[rows + 1, cols - 1] -= matrix[rows, cols];
+            }
+            if (IsInRange(matrix, rows + 1, cols + 1) && matrix[rows + 1, cols + 1] > 0)
+            {
+                matrix[rows + 1, cols + 1] -= matrix[rows, cols];
+            }
+            matrix[rows, cols] = 0;
+        }
+        private static bool IsInRange(int[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+        }
+
+        private static int[,] ReadMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            return matrix;
+        }
+    }
+}
