7e461692c64731f7d6b21763987f523fc410d755
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-04-22 13:16:53 +0300

    Singleton Pattern

---
+++ Design Patterns/Singleton Pattern/Singleton Monostate/Car.cs
@@ -0,0 +1,32 @@
+﻿namespace SingletonMonostate
+{
+    public class Car
+    {
+        private static string manufacture; 
+
+        private static int year;
+
+        private static string model;
+
+        public string Manufacture
+        {
+            get => manufacture;
+            set => manufacture = value;
+        }
+        public int Year
+        {
+            get => year;
+            set => year = value;
+        }
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Manufacture} {this.Model} is manufactured in {this.Year}";
+        }
+    }
+}
---
+++ Design Patterns/Singleton Pattern/Singleton Monostate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingletonMonostate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Manufacture = "Audi";
+            car.Model = "A3";
+            car.Year = 2010;
+
+            Console.WriteLine(car);
+
+            Car car2 = new Car();
+            car2.Year = 2011;
+
+            //Both cars are with same refference and equal
+            Console.WriteLine(car);
+            Console.WriteLine(car2);
+        }
+    }
+}
