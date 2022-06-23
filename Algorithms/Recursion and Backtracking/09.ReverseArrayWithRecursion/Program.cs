c55b0f4dfd090f4c38245716f65dec3d14dd6d26
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-06-23 13:24:26 +0300

    Initial Commit

---
+++ Algorithms/Recursion and Backtracking/09.ReverseArrayWithRecursion/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var array = Console.ReadLine().Split();
+
+ReverseArray(array, 0);
+Console.WriteLine(string.Join(" ", array));
+
+void ReverseArray(string[] array, int index)
+{
+    var reversedArr = new string[array.Length];
+
+    if (index == array.Length / 2)
+    {
+        return;
+    }
+
+    var temp = array[index];
+    array[index] = array[array.Length - index - 1];
+    array[array.Length - index - 1] = temp;
+}
---
+++ Algorithms/Recursion and Backtracking/10.NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _19.NestedLoopsToRecursion
+{
+    public class Program
+    {
+        private static int[] arr;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            arr = new int[n];
+            GenerateNumbers(0);
+        }
+
+        private static void GenerateNumbers(int index)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(String.Join(" ", arr));
+                return;
+            }
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                arr[index] = i;
+                GenerateNumbers(index + 1);
+            }
+        }
+    }
+}
