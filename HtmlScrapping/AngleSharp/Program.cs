10ab7fd5ea3467b24101732814b088698c1f1f7c
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-08-17 17:19:46 +0300

    HtmlScrapping

---
+++ HtmlScrapping/AngleSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using AngleSharp;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        //Scrapping information from TopGear using AngleSharp only for educational purposes!
+        var config = Configuration.Default.WithDefaultLoader();
+        var context = BrowsingContext.New(config);
+
+        var document = await context.OpenAsync("https://www.topgear.com/car-news/electric/top-gears-top-20-electric-cars");
+
+        // get top 20 electric cars
+        var electricCarsBrands = document.QuerySelectorAll("#main > div > ol > li > div.sc-bdfBQB.cylQah > div.sc-bdfBQB.foPALk > h2"); 
+        //copy selecto and get the path
+
+        var descriptions = document.QuerySelectorAll("#main > div > ol > li > div.sc-bdfBQB.cylQah > div.sc-bdfBQB.ghikhw > div > p:nth-child(1)");
+
+        var imagesLinks = document.QuerySelectorAll("#main > div > ol > li > div.sc-bdfBQB.cylQah > div.sc-bdfBQB.hEhhZA > div > div > img");
+
+        var listLinks = document.QuerySelectorAll("#main > div > ol > li > div > div.sc-bdfBQB.ghikhw > div > p:nth-child(2) > a");
+
+        foreach (var carBrand in electricCarsBrands)
+        {
+            Console.WriteLine(carBrand.TextContent);
+            Console.WriteLine("------------");
+        }
+
+        Console.WriteLine();
+
+        foreach (var listLink in listLinks)
+        {
+            Console.WriteLine(listLink.GetAttribute("href"));
+        }
+
+        Console.WriteLine();
+
+        foreach (var desc in descriptions)
+        {
+            Console.WriteLine(desc.TextContent);
+            Console.WriteLine("------------");
+           //Console.WriteLine(item.InnerHtml);
+           //Console.WriteLine(item.OuterHtml);
+           //Console.WriteLine(item.ToHtml());
+           //Console.WriteLine(item);
+        }
+
+        Console.WriteLine();
+
+        foreach (var image in imagesLinks)
+        {
+          Console.WriteLine("https://www.topgear.com/" + image.GetAttribute("src"));
+        }
+    }
+}
---
+++ HtmlScrapping/HtmlAgilityPack/Program.cs
@@ -0,0 +1,30 @@
+﻿// From File
+using HtmlAgilityPack;
+using System.Text;
+
+internal class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        var url = "https://recepti.gotvach.bg/r-225119";
+
+        using (HttpClient client = new HttpClient())
+        {
+            using (HttpResponseMessage response = client.GetAsync(url).Result)
+            {
+                using (HttpContent content = response.Content)
+                {
+                    string result = content.ReadAsStringAsync().Result;
+                    var htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(result);
+
+                    Console.WriteLine(htmlDocument.DocumentNode.InnerText);
+
+                    Console.WriteLine(htmlDocument.ToString());
+                }
+            }
+        }
+    }
+}
